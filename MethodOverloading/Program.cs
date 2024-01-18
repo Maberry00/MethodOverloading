e1b68592e495303412f806176f651ec3bde0b227
Maberry00 <maberry30@gmail.com>
2024-01-18 13:14:34 -0600

    Add

--- MethodOverloading/Program.cs
+++ MethodOverloading/Program.cs
@@ -4,7 +4,37 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            //Add(10, 20);
+            //Add(2.2, 5.5);
+            //Add(5, 3, true);
         }
+
+        public static void Add(int num1, int num2)
+        {
+            Console.WriteLine(num1 + num2);
+        }
+
+        public static void Add(double num1, double num2)
+        { 
+            Console.WriteLine(num1 + num2);
+        }
+
+        public static void Add(int num1, int num2, bool Dollars)
+        {
+            if (Dollars && (num1 + num2) !=1)
+            {
+                Console.WriteLine($"{num1 + num2} dollars");
+            }
+            else if (Dollars && (num1 + num2) ==1) 
+            {
+                Console.WriteLine($"{num1+num2} dollars");
+            }
+            else 
+            {
+                Console.WriteLine($"{num1+num2}");
+            }
+
+        }
+
     }
 }
